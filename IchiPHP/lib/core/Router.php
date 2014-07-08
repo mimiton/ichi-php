74a2683d07930cf3018e4993c4a9fdbe70c11b90
mimiton <xiaozheen@qq.com>
2014-07-08 08:08:14 +0800

    添加路由过程中的异常处理

--- IchiPHP/lib/core/Router.php
+++ IchiPHP/lib/core/Router.php
@@ -7,13 +7,41 @@
 class Router {
 	
 	/**
+	 * @desc  调用路由方法，并捕捉异常
+	 * @param unknown $uri             URI
+	 *        boolean $handleException 是否处理异常
+	 */
+	static function to( $uri, $handleException = true ) {
+		
+		try {
+			
+			// 返回false，则抛出异常
+			if( !self::routeToController( $uri ) )
+				throw new Exception();
+			
+		} catch( Exception $e ) {
+			
+			// 处理异常
+			if( $handleException )
+				self::handleException();
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
 	 * @desc  根据uri路由到匹配的可用的控制器上
 	 * @param unknown $uri
+	 * @return boolean
 	 */
-	static function to( $uri ) {
-		
+	private static function routeToController( $uri ) {
 		// 控制器目录
-		$path = getcwd() . '/'. ICHI_PHP_PATH .'/app/controllers/';
+		$path = ICHI_CONTROLLERS_PATH;
+		
+		// 控制器命名空间
+		$nameSpace = ICHI_CONTROLLERS_NS;
 		
 		// 用 `/` 拆解uri
 		$matches = explode( '/', $uri );
@@ -25,30 +53,38 @@ class Router {
 			
 			$path .= $name;
 			
+			$nameSpace .= $name;
+			
 			// 控制器已经找到
 			// 剩下部分的uri做调用控制器的参数使用
 			if( isset($controller) ) {
 
 				// 调用控制器方法
-				Router::callFn( $controller, $name, array_slice($matches, $i) );
+				return Router::callFn( $controller, $name, array_slice($matches, $i) );
 				
-				return;
 			}
 			
 			// 判断对应文件是否存在
 			else if( file_exists( $path . '.php' ) ) {
 				// 包含文件并创建控制器
 				require_once $path . '.php';
-				$controller = new $name();
+				if( class_exists($nameSpace) )
+					$controller = new $nameSpace();
+				else
+					$controller = new $name();
 			}
 			
+			// 进入下一层目录
 			$path .= '/';
+			
+			// 进入下一层命名空间
+			$nameSpace .= '\\';
 			
 		}
 		
 		// uri没有下一层了
 		// 调用控制器的_default方法
-		Router::callFn( $controller, '_default' );
+		return Router::callFn( $controller, '_default' );
 		
 	}
 	
@@ -57,9 +93,9 @@ class Router {
 	 * @param unknown $controller
 	 * @param unknown $fnName
 	 */
-	static function callFn( $controller, $fnName, $args = NULL ) {
+	private static function callFn( $controller, $fnName, $args = NULL ) {
 		
-		if( !isset($controller) ) return;
+		if( !isset($controller) ) return false;
 		
 		// 方法存在
 		if( method_exists( $controller, $fnName ) )
@@ -72,6 +108,19 @@ class Router {
 		// 默认方法
 		else if( method_exists( $controller, '_default' ) )
 			$controller->_default( new Request(), new Response(), $args );
+		else
+			return false;
+		
+		return true;
+	}
+	
+	/**
+	 * @desc  处理异常，路由至404页面对应控制器
+	 */
+	private static function handleException() {
+		
+		if( !self::to( '/_default/_404', false ) )
+			echo 'no controller & function to handle this request';
 		
 	}
 }
