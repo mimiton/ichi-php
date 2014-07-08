4044c7819d2ea18af421583035c58d453ef0e601
mimiton <xiaozheen@qq.com>
2014-07-08 17:59:39 +0800

    模型调用类

---
+++ IchiPHP/lib/core/Model.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @desc   模型调用类
+ * @author xiaozheen
+ *
+ */
+class Model {
+	
+	/**
+	 * @desc  载入数据模型
+	 * @param unknown $modelPath
+	 * @return unknown
+	 */
+	static function load( $modelPath, $autoInstantiate = false ) {
+		
+		// 文件路径
+		$filePath = ICHI_MODELS_PATH . $modelPath . '.php';
+		
+		// 类的命名空间
+		$nameSpace = ICHI_MODELS_NS . str_replace( '/', '\\', $modelPath );
+		
+		// 载入文件
+		require_once $filePath;
+		
+		// 默认不创建实例
+		if( !$autoInstantiate )
+			return;
+		
+		// 用命名空间创建实例
+		if( class_exists($nameSpace) )
+			$model = new $nameSpace();
+		
+		// 降级尝试使用文件名创建实例
+		else {
+			$matches = explode( '\\', $modelPath );
+			$className = $matches[ count($matches)-1 ];
+			$model = new $className();
+		}
+		
+		return $model;
+		
+	}
+}
