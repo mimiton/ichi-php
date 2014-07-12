299ef49200ee6bed85adac4cf486fc940d025827
mimiton <xiaozheen@qq.com>
2014-07-13 00:08:29 +0800

    添加三个判断浏览器信息的方法

--- IchiPHP/lib/core/Request.php
+++ IchiPHP/lib/core/Request.php
@@ -12,6 +12,62 @@ class Request {
 		
 		// 请求参数
 		$this->params = $_REQUEST;
+
+	}
+	
+	/**
+	 * @desc   获取浏览器信息
+	 * @return multitype:unknown string
+	 */
+	function getBrowserInfo() {
+		
+		$UA = $_SERVER['HTTP_USER_AGENT'];
+		
+		if( !preg_match('/(chrome|safari|opera)\/([\d\.]+)/i', $UA, $matches) )
+			preg_match('/(MSIE)\s*([\d\.]+)/i', $UA, $matches);
+		
+		return array(
+			'name'    => strtolower( $matches[1] ),
+			'version' => $matches[2]
+		);
+		
+	}
+	
+	/**
+	 * @desc   判断旧IE
+	 * @param  number $version
+	 * @return boolean
+	 */
+	function fromOldIE( $versionLimit = 8 ) {
+		
+		$browserInfo = $this->getBrowserInfo();
+		
+		if( $browserInfo['name'] == 'msie' ) {
+
+			$version = explode( '.', $browserInfo['version'] );
+			$version = $version[0];
+			
+			if( $version < $versionLimit )
+				return true;
+			
+		}
+		
+		return false;
+		
+	}
+	
+	/**
+	 * @desc   判断来自移动设备
+	 * @return boolean
+	 */
+	function fromMobileDevice() {
+		
+		$UA = $_SERVER['HTTP_USER_AGENT'];
+		
+		if( preg_match('/android|iphone|ipod|ucweb/i', $UA) )
+			return true;
+		else
+			return false;
 		
 	}
 	
