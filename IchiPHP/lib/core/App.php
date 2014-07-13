4fc0738f20b972816c9c052dfb5195757ab843af
mimiton <xiaozheen@qq.com>
2014-07-13 18:17:31 +0800

    添加同框架多应用支持

--- IchiPHP/lib/core/App.php
+++ IchiPHP/lib/core/App.php
@@ -6,6 +6,18 @@ class App {
 	
 	// 路由器
 	private static $router;
+	// 应用路由表
+	private static $appMap;
+	
+	/**
+	 * @desc  设置应用路由表
+	 * @param unknown $map
+	 */
+	static function setAppMap( $map ) {
+		
+		self::$appMap = $map;
+		
+	}
 	
 	/**
 	 * @desc  启动App
@@ -21,9 +33,40 @@ class App {
 	 */
 	private static function route() {
 
+		// 当前来源域名
+		$domain = $_SERVER['SERVER_NAME'];
+		
+		// 获取对应app的名字
+		$appName = self::getAppNameByDomain($domain);
+		
+		// 路由参数
+		$routerConfig = array(
+				'appName' => $appName
+		);
+		
+		// 设置路由参数
+		Router::config( $routerConfig );
+		
+		// 去掉GET参数
 		$reqUri = strtok( $_SERVER['REQUEST_URI'], '?' );
 		
+		// 调用路由器
 		Router::to( $reqUri );
+		
+	}
+	
+	/**
+	 * @desc  以正则匹配方式获取域名对应的应用名
+	 * @param unknown $domain
+	 * @return unknown|string
+	 */
+	private static function getAppNameByDomain( $domain ) {
+		
+		foreach ( self::$appMap as $pattern => $appName )
+			if( preg_match( '/^' . $pattern . '$/i', $domain ) )
+				return $appName;
+		
+		return '';
 		
 	}
 
