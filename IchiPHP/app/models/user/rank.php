a6efcc2c8e630173252662a52d2fb72e1fae880c
mimiton <xiaozheen@qq.com>
2014-07-13 18:18:06 +0800

    update

--- IchiPHP/app/models/user/rank.php
+++ IchiPHP/app/models/user/rank.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace app\models\user;
+use \SQL;
+use \Driver;
 
 class rank {
 	
 	function getRank( $uid ) {
 		
-		$mysql = \Driver::useDefault('mysql');
+		$mysql = Driver::useDefault('mysql');
 		
-		$sql = \SQL::table('tb_usr')->select('*')->where('id', '>=', 1)->get();
+		$sql = SQL::table('tb_usr')->select('*')->where('id', '>=', 1)->get();
 		
 		return $mysql->getData($sql);
 		
