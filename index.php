74735ad537a6b593f5cd63c5802c739363d42e3f
mimiton <xiaozheen@qq.com>
2014-07-13 18:17:55 +0800

    添加应用路由表配置示例

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ error_reporting(E_ALL &~ (E_STRICT | E_NOTICE));
 // 载入核心库
 require 'IchiPHP/lib/lib.php';
 
+// 配置应用路由表，域名正则 => 应用名
+App::setAppMap(array(
+'ichiphp.com' => 'ichiphp'
+));
+
 // 配置别名为mysql的预设数据库
 // 在模型类里使用 /Driver::useDefault(别名); 载入
 Driver::config(array(
