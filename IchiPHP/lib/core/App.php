7ad593bd433bca4be64c489ab4274940d1f3a842
mimiton <xiaozheen@qq.com>
2014-07-20 01:03:50 +0800

    添加特殊路由的支持

--- IchiPHP/lib/core/App.php
+++ IchiPHP/lib/core/App.php
@@ -4,20 +4,17 @@
  */
 class App {
 	
+	// 请求方法常量
+	const REQ_METHOD_ALL = 'ALL';
+	const REQ_METHOD_GET = 'GET';
+	const REQ_METHOD_POST= 'POST';
+	const REQ_METHOD_PUT = 'PUT';
+	const REQ_METHOD_DEL = 'DELETE';
+	
 	// 路由器
 	private static $router;
-	// 应用路由表
-	private static $appMap;
-	
-	/**
-	 * @desc  设置应用路由表
-	 * @param unknown $map
-	 */
-	static function setAppMap( $map ) {
-		
-		self::$appMap = $map;
-		
-	}
+	// 域名应用配置
+	static $domainConfigs = array();
 	
 	/**
 	 * @desc  启动App
@@ -25,6 +22,17 @@ class App {
 	static function run() {
 		
 		self::route();
+		
+	}
+
+	/**
+	 * @desc   开启域名路由配置链式操作
+	 * @param  unknown $domain
+	 * @return DomainConfigSetter
+	 */
+	static function onDomain( $domain ) {
+		
+		return new DomainConfigSetter( $domain );
 		
 	}
 	
@@ -36,15 +44,10 @@ class App {
 		// 当前来源域名
 		$domain = $_SERVER['SERVER_NAME'];
 		
-		// 获取对应app的名字
-		$appName = self::getAppNameByDomain($domain);
+		// 获取域名对应的应用配置
+		$routerConfig = self::getRouterConfigByDomain( $domain );
 		
-		// 路由参数
-		$routerConfig = array(
-				'appName' => $appName
-		);
-		
-		// 设置路由参数
+		// 设置路由器参数
 		Router::config( $routerConfig );
 		
 		// 去掉GET参数
@@ -56,19 +59,61 @@ class App {
 	}
 	
 	/**
-	 * @desc  以正则匹配方式获取域名对应的应用名
-	 * @param unknown $domain
+	 * @desc   以正则匹配方式获取域名对应应用的路由配置
+	 * @param  unknown $domain
 	 * @return unknown|string
 	 */
-	private static function getAppNameByDomain( $domain ) {
+	private static function getRouterConfigByDomain( $domain ) {
 		
-		foreach ( self::$appMap as $pattern => $appName )
+		foreach ( self::$domainConfigs as $pattern => $config )
 			if( preg_match( '/^' . $pattern . '$/i', $domain ) )
-				return $appName;
+				return $config;
 		
-		return '';
+		return NULL;
 		
 	}
+	
+}
 
+/**
+ * @desc   域名配置链式操作工具
+ * @author xiaozheen
+ */
+class DomainConfigSetter {
+	
+	function __construct( $domain ) {
+		$this->domain = $domain;
+	}
+	
+	function toApp( $appName ) {
+		
+		$domain = $this->domain;
+		
+		App::$domainConfigs[ $domain ]['appName'] = $appName;
+		
+		return $this;
+	}
+	
+	private function specialRoute( $method, $uriRegex, $reRouteUri ) {
 
+		$domain = $this->domain;
+		
+		App::$domainConfigs[ $domain ]['specialRoute'][ $method ][ $uriRegex ] = $reRouteUri;
+		
+		return $this;
+		
+	}
+	
+	function all( $uriRegex, $reRouteUri ) {
+		
+		return $this->specialRoute( App::REQ_METHOD_ALL, $uriRegex, $reRouteUri );
+		
+	}
+	
+	function get( $uriRegex, $reRouteUri ) {
+		
+		return $this->specialRoute( App::REQ_METHOD_GET, $uriRegex, $reRouteUri );
+	
+	}
+	
 }
