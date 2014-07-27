39037e53a38044a01a214d780de60136d47bb229
mimiton <xiaozheen@qq.com>
2014-07-27 12:24:25 +0800

    添加Response类对视图引擎的调用

--- IchiPHP/lib/core/Response.php
+++ IchiPHP/lib/core/Response.php
@@ -6,11 +6,27 @@
  */
 class Response {
 	
+	private static $viewEngineName = 'IchiViewEngine';
+	private static $viewPath;
+	private static $viewEngine;
+	
+	/**
+	 * @desc  设置视图（模板）引擎
+	 * @param unknown $engine
+	 */
+	static function setViewEngine( $engineName ) {
+		self::$viewEngineName = $engineName;
+	}
+	
+	static function setViewPath( $path ) {
+		self::$viewPath = $path;
+	}
+	
 	/**
 	 * @desc  直接输出字符
 	 * @param unknown $data
 	 */
-	function write( $data ) {
+	static function write( $data ) {
 		echo $data;
 	}
 	
@@ -20,15 +36,42 @@ class Response {
 	 * @param unknown $key
 	 * @param unknown $value
 	 */
-	function assign( $key, $value ) {
-		
+	static function assign( $key, $value ) {
+		self::initEngine();
+		self::$viewEngine->assign( $key, $value );
 	}
 	
 	/**
 	 * @desc  渲染模板
 	 * @param unknown $tplPath
 	 */
-	function render( $tplPath ) {
+	static function render( $tplRelativePath ) {
+		self::initEngine();
+		
+		if( !isset(self::$viewPath) )
+			self::$viewPath = ICHI_APP_PATH . '/views/';
+		
+		self::$viewEngine->render( self::$viewPath . $tplRelativePath );
+	}
+	
+	/**
+	 * @desc   初始化模板引擎
+	 * @throws IchiStatusException
+	 */
+	private static function initEngine() {
+		
+		if( !isset(self::$viewEngine) ) {
+			
+			// 引擎名
+			$engineName = self::$viewEngineName;
+			
+			// 文件路径
+			$fileRelativePath = '/viewengine/' . $engineName;
+			
+			// 载入驱动文件并创建实例
+			self::$viewEngine = Driver::load( $fileRelativePath, true );
+			
+		}
 		
 	}
 }
