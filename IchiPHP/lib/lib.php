bfbef8bbabe8107938472dd174a5f436870a5cda
mimiton <xiaozheen@qq.com>
2014-07-27 12:24:55 +0800

    整理

--- IchiPHP/lib/lib.php
+++ IchiPHP/lib/lib.php
@@ -1,7 +1,10 @@
 <?php
 
 // 默认加密密匙
-defined('ICHI_SALT') or define('ICHI_SALT', '18dh237ddjw9');
+defined('ICHI_SALT') or define('ICHI_SALT', 'iu87sja01kgnvkd2');
+
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** **  文件路径 ** ** ** ** ** ** ** ** ** ** ** ** ** **/
 
 // 当前工作目录
 defined('ICHI_CWD') or define('ICHI_CWD', getcwd());
@@ -9,7 +12,7 @@ defined('ICHI_CWD') or define('ICHI_CWD', getcwd());
 defined('ICHI_PHP_PATH')         or define('ICHI_PHP_PATH',         ICHI_CWD . '/IchiPHP');
 
 // 定义 app 目录
-defined('ICHI_APP_PATH')         or define('ICHI_APP_PATH',         ICHI_PHP_PATH . '/app');
+defined('ICHI_APP_PATH')         or define('ICHI_APP_PATH',         ICHI_CWD . '/app');
 // 定义 extend 目录
 defined('ICHI_EXTEND_PATH')     or define('ICHI_EXTEND_PATH',       ICHI_PHP_PATH . '/extend');
 
@@ -20,6 +23,12 @@ defined('ICHI_MODELS_PATH')      or define('ICHI_MODELS_PATH',      ICHI_APP_PAT
 // 定义 驱动 目录
 defined('ICHI_DRIVERS_PATH')     or define('ICHI_DRIVERS_PATH',     ICHI_EXTEND_PATH . '/drivers');
 
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **/
+
+
+
+/** ** ** ** ** ** ** ** ** ** ** ** ** **  命名空间 ** ** ** ** ** ** ** ** ** ** ** ** ** **/
+
 // 定义 控制器 命名空间
 defined('ICHI_CONTROLLERS_NS')   or define('ICHI_CONTROLLERS_NS',   '\app\controllers');
 // 定义 模型 命名空间
@@ -27,10 +36,12 @@ defined('ICHI_MODELS_NS')        or define('ICHI_MODELS_NS',        '\app\models
 // 定义 驱动 命名空间
 defined('ICHI_DRIVERS_NS')       or define('ICHI_DRIVERS_NS',       '\extend\drivers');
 
+/** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **/
 
 
 
 // 包含各模块
+require 'core/Interfaces.php';
 require 'core/SQL.php';
 require 'core/Cookie.php';
 require 'core/Session.php';
