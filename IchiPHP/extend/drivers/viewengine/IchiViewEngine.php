98bba770fc88c52cd6e72263cd9d6bc5608e865b
mimiton <xiaozheen@qq.com>
2014-07-27 12:23:51 +0800

    默认的简单视图引擎

---
+++ IchiPHP/extend/drivers/viewengine/IchiViewEngine.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace extend\drivers\viewengine;
+
+/**
+ * @desc   简单的默认视图引擎
+ * @author xiaozheen
+ *
+ */
+class IchiViewEngine implements \IViewEngine {
+	
+	// 视图变量
+	private $vars = array();
+	
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see IViewEngine::assign()
+	 */
+	function assign( $key, $value ) {
+		$this->vars[ $key ] = $value;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see IViewEngine::render()
+	 */
+	function render( $tplPath ) {
+		
+		foreach ( $this->vars as $k => $v )
+			$$k = $v;
+		
+		include $tplPath;
+		
+	}
+	
+}
+
+?>
