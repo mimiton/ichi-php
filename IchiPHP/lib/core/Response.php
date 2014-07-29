ffb3dca11a296a20dced6a39ccdb0fee8290d8a6
mimiton <xiaozheen@qq.com>
2014-07-29 14:58:25 +0800

    添加assign方法对 `多个键值对` 的支持

--- IchiPHP/lib/core/Response.php
+++ IchiPHP/lib/core/Response.php
@@ -18,6 +18,10 @@ class Response {
 		self::$viewEngineName = $engineName;
 	}
 	
+	/**
+	 * @desc  设置视图文件目录
+	 * @param unknown $path
+	 */
 	static function setViewPath( $path ) {
 		self::$viewPath = $path;
 	}
@@ -36,9 +40,21 @@ class Response {
 	 * @param unknown $key
 	 * @param unknown $value
 	 */
-	static function assign( $key, $value ) {
-		self::initEngine();
-		self::$viewEngine->assign( $key, $value );
+	static function assign( $key, $value = NULL ) {
+		
+		// 初始化视图引擎实例
+		self::initViewEngine();
+		
+		// 设置视图变量键值对
+		if( !is_array( $key ) ) {
+			$keyValues = array( $key => $value );
+		}
+		else
+			$keyValues = $key;
+		
+		foreach ( $keyValues as $k => $v )
+			self::$viewEngine->assign( $k, $v );
+		
 	}
 	
 	/**
@@ -46,19 +62,25 @@ class Response {
 	 * @param unknown $tplPath
 	 */
 	static function render( $tplRelativePath ) {
-		self::initEngine();
 		
+		// 初始化视图引擎实例
+		self::initViewEngine();
+		
+		// 默认视图文件路径为：app目录下views目录
+		// app目录取决于全局常量ICHI_APP_PATH的定义
 		if( !isset(self::$viewPath) )
 			self::$viewPath = ICHI_APP_PATH . '/views/';
 		
+		// 调用引擎实例的render方法
 		self::$viewEngine->render( self::$viewPath . $tplRelativePath );
+		
 	}
 	
 	/**
 	 * @desc   初始化模板引擎
 	 * @throws IchiStatusException
 	 */
-	private static function initEngine() {
+	private static function initViewEngine() {
 		
 		if( !isset(self::$viewEngine) ) {
 			
