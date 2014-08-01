07f2cfa9cf876a21d1940e360fb7e29ee41cfe63
mimiton <xiaozheen@qq.com>
2014-08-01 08:45:52 +0800

    移出DomainConfiger类，调整路由器配置过程

--- IchiPHP/lib/core/App.php
+++ IchiPHP/lib/core/App.php
@@ -3,24 +3,30 @@
  * @desc   App类
  */
 class App {
-	
-	// 请求方法常量
-	const REQ_METHOD_ALL = 'ALL';
-	const REQ_METHOD_GET = 'GET';
-	const REQ_METHOD_POST= 'POST';
-	const REQ_METHOD_PUT = 'PUT';
-	const REQ_METHOD_DEL = 'DELETE';
-	
-	// 路由器
-	private static $router;
-	// 域名应用配置
-	static $domainConfigs = array();
-	
+
 	/**
 	 * @desc  启动App
 	 */
 	static function run() {
-		
+
+        // 当前来源域名
+        $domain         = $_SERVER['SERVER_NAME'];
+
+        // 一个域名配置器
+        $domainConfiger = new DomainConfiger( $domain );
+
+        // 获取匹配的应用名
+        $appName        = $domainConfiger->getAppName();
+
+        // 获取特殊路由规则
+        $specialRoute   = $domainConfiger->getSpecialRoute();
+
+        // 设置路由器参数
+        Router::setAppName( $appName );
+        Router::setSpecialRoute( $specialRoute );
+
+        Router::to( '/_default/_app', false );
+        // 开始路由
 		self::route();
 		
 	}
@@ -31,24 +37,16 @@ class App {
 	 * @return DomainConfigSetter
 	 */
 	static function onDomain( $domain ) {
-		
-		return new DomainConfigSetter( $domain );
-		
+
+        // 返回一个域名配置器给外部使用
+		return new DomainConfiger( $domain );
+
 	}
 	
 	/**
 	 * @desc  进行路由
 	 */
 	private static function route() {
-
-		// 当前来源域名
-		$domain = $_SERVER['SERVER_NAME'];
-		
-		// 获取域名对应的应用配置
-		$routerConfig = self::getRouterConfigByDomain( $domain );
-		
-		// 设置路由器参数
-		Router::config( $routerConfig );
 		
 		// 去掉GET参数
 		$reqUri = strtok( $_SERVER['REQUEST_URI'], '?' );
@@ -56,123 +54,6 @@ class App {
 		// 调用路由器
 		Router::to( $reqUri );
 		
-	}
-	
-	/**
-	 * @desc   以正则匹配方式获取域名对应应用的路由配置
-	 * @param  unknown $domain
-	 * @return unknown|string
-	 */
-	private static function getRouterConfigByDomain( $domain ) {
-		
-		foreach ( self::$domainConfigs as $pattern => $config )
-			if( preg_match( '/^' . $pattern . '$/i', $domain ) )
-				return $config;
-		
-		return NULL;
-		
-	}
-	
-}
-
-/**
- * @desc   链式操作工具，配置域名->应用的映射，以及特殊路由规则
- * @author xiaozheen
- */
-class DomainConfigSetter {
-	
-	/**
-	 * @desc  构造方法，传入要配置的目标域名
-	 * @param unknown $domain
-	 */
-	function __construct( $domain ) {
-		$this->domain = $domain;
-	}
-	
-	/**
-	 * @desc   映射到指定应用
-	 * @param  unknown $appName
-	 * @return DomainConfigSetter
-	 */
-	function toApp( $appName ) {
-		
-		$domain = $this->domain;
-		
-		App::$domainConfigs[ $domain ]['appName'] = $appName;
-		
-		return $this;
-	}
-	
-	/**
-	 * @desc  添加特殊路由
-	 * @param unknown $method
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 * @return DomainConfigSetter
-	 */
-	private function specialRoute( $method, $uriRegex, $reRouteUri ) {
-
-		$domain = $this->domain;
-		
-		App::$domainConfigs[ $domain ]['specialRoute'][ $method ][ $uriRegex ] = $reRouteUri;
-		
-		return $this;
-		
-	}
-	
-	/**
-	 * @desc  添加处理所有（ALL）请求类型的特殊路由
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 */
-	function all( $uriRegex, $reRouteUri ) {
-		
-		return $this->specialRoute( App::REQ_METHOD_ALL, $uriRegex, $reRouteUri );
-		
-	}
-	
-	/**
-	 * @desc  添加处理（GET）请求类型的特殊路由
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 */
-	function get( $uriRegex, $reRouteUri ) {
-		
-		return $this->specialRoute( App::REQ_METHOD_GET, $uriRegex, $reRouteUri );
-	
-	}
-
-	/**
-	 * @desc  添加处理（POST）请求类型的特殊路由
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 */
-	function post( $uriRegex, $reRouteUri ) {
-	
-		return $this->specialRoute( App::REQ_METHOD_POST, $uriRegex, $reRouteUri );
-	
-	}
-
-	/**
-	 * @desc  添加处理（PUT）请求类型的特殊路由
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 */
-	function put( $uriRegex, $reRouteUri ) {
-	
-		return $this->specialRoute( App::REQ_METHOD_PUT, $uriRegex, $reRouteUri );
-	
-	}
-
-	/**
-	 * @desc  添加处理（DELETE）请求类型的特殊路由
-	 * @param unknown $uriRegex
-	 * @param unknown $reRouteUri
-	 */
-	function delete( $uriRegex, $reRouteUri ) {
-	
-		return $this->specialRoute( App::REQ_METHOD_DELETE, $uriRegex, $reRouteUri );
-	
 	}
 	
 }
