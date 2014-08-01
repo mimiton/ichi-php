42b0a7dc123521c27babd7db470c074c948aafe4
mimiton <xiaozheen@qq.com>
2014-08-01 08:44:51 +0800

    应用预处理控制器demo

---
+++ app/controllers/ichiphp/_default/_app.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaozheen
+ * Date: 14-8-1
+ * Time: 上午8:35
+ */
+
+namespace controllers\ichiphp\_default;
+
+/**
+ * Class _app
+ * @package controllers\ichiphp\_default
+ * @desc 应用预处理控制器，进行正式的路由之前，App类会调用路由器路由至此处_default方法执行一次
+ */
+class _app {
+
+    function _default() {
+        // TODO: 配置全局变量、初始化Session、...
+    }
+
+} 
