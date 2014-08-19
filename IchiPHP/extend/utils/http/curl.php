d2c4c3f833cab31ea5ccb8bdca9fdb3b1651c6b0
mimiton <xiaozheen@qq.com>
2014-08-19 17:31:22 +0800

    添加CURL

---
+++ IchiPHP/extend/utils/http/curl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaozheen
+ * Date: 14-8-19
+ * Time: 下午2:02
+ */
+
+namespace extend\utils\http;
+
+
+class curl {
+
+    function get( $url, $data, $timeout = 10, $proxy = NULL ) {
+
+    }
+
+    function post( $url, $data, $timeout = 10, $proxy = NULL ) {
+
+    }
+
+    function put( $url, $data, $timeout = 10, $proxy = NULL ) {
+
+    }
+
+    function delete( $url, $data, $timeout = 10, $proxy = NULL ) {
+
+    }
+
+} 
