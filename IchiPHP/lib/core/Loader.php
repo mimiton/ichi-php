369a6fbebaa04669faefe4da9caed39e6070bd6f
mimiton <xiaozheen@qq.com>
2014-10-26 23:46:44 +0800

    优化加载过程

--- IchiPHP/lib/core/Loader.php
+++ IchiPHP/lib/core/Loader.php
@@ -6,6 +6,9 @@
  * Time: 下午10:30
  */
 
+/**
+ * @desc 文件加载类Loader
+ */
 class Loader {
 
     /**
@@ -13,35 +16,31 @@ class Loader {
      */
     static function load( $realPathRoot, $nameSpaceRoot, $abstractPath, $autoInstatiate = false ) {
 
-        // 文件真实路径
-        $filePath  = $realPathRoot . $abstractPath . '.php';
+        // 模块命名空间
+        $nameSpace = $nameSpaceRoot . str_replace( '/', '\\', $abstractPath );
 
-        // 文件不存在，抛出500异常，由Router类抓取
-        if( !file_exists($filePath) )
-            throw new IchiStatusException( 500, 'File of abstract path: `'.$abstractPath.'` not found!' );
+        // 若命名空间下模块已存在，则跳过文件加载
+        if( !class_exists($nameSpace) ) {
 
-        // 载入文件
-        require_once $filePath;
+            // 文件真实路径
+            $filePath  = $realPathRoot . $abstractPath . '.php';
+
+            // 文件不存在，抛出500异常，由Router类抓取
+            if( !file_exists($filePath) )
+                throw new IchiStatusException( 500, 'Loader::load(): File of abstract path `'.$abstractPath.'` not found!' );
+
+            // 载入文件
+            require_once $filePath;
+
+        }
 
         // 非自动创建模式，返回
         if( !$autoInstatiate )
             return;
 
-        // 模块命名空间
-        $nameSpace = $nameSpaceRoot . str_replace( '/', '\\', $abstractPath );
+        // 返回创建的实例
 
-        // 尝试使用命名空间创建实例
-        if( class_exists($nameSpace) )
-            $driver = new $nameSpace();
-        // 降级，使用文件名创建实例
-        // 从文filePath中获取文件名：\foo\bar\abc => new abc();
-        else {
-            $matches = explode( '\\', $abstractPath );
-            $className = $matches[ count($matches)-1 ];
-            $driver = new $className();
-        }
-
-        return $driver;
+        return new $nameSpace();
 
     }
 
