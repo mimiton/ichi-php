2629c47d820114549609e3c5275ce5832eee2087
mimiton <xiaozheen@qq.com>
2015-06-02 22:37:09 +0800

    test

--- config.php
+++ config.php
@@ -28,6 +28,6 @@ Driver::config(
 
 // 配置域名路由，域名正则 => 应用名
 // 不同域名对应不同的控制器目录，干净地管理控制器路由规则！
-App::onDomain('ichi[ph]{3}.com')->toApp('ichiphp')
+App::onDomain('.+')->toApp('ichiphp')
 ->all('/shop(\d+)','/a/one/bar/$1')  // 将所有URI匹配`/shop(\d+)`请求的请求转到`/a/one/bar/$1`对应的控制器
 ->get('/food-(\w+)','/b/foo/$1');    // 将URI匹配`/food-(\w+)`的GET请求转到`/b/foo/$1`的控制器
